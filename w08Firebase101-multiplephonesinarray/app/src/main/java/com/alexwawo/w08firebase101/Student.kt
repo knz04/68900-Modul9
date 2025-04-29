4e9615ed000edd3dcff7daef8a08e6c28459e107
hawyeet <53439203+knz04@users.noreply.github.com>
2025-04-29 22:54:32 +0700

    Bagian 2, checkpoint 1

--- w08Firebase101-multiplephonesinarray/app/src/main/java/com/alexwawo/w08firebase101/Student.kt
+++ w08Firebase101-multiplephonesinarray/app/src/main/java/com/alexwawo/w08firebase101/Student.kt
@@ -4,6 +4,7 @@ data class Student(
     val id: String = "",
     val name: String = "",
     val program: String = "",
-    val phones: List<String> = emptyList()
+    val phones: List<String> = emptyList(),
+    val docId: String = ""
 )
 
